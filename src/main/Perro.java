acffe8fa5e6adb3941f43367c08c10e7d1bb86d1
Cristian Vidal <Cristian.vidal.lopez@gmail.com>
2020-11-27 18:24:21 -0300

    ADD: Cambios en perro.java, nuevo atributo edad.

--- src/main/Perro.java
+++ src/main/Perro.java
@@ -3,10 +3,12 @@ package main;
 public class Perro {
 	
 	private String name;
+	private int edad;
 
-	public Perro(String name) {
+	public Perro(String name, int edad) {
 		super();
 		this.name = name;
+		this.edad = edad;
 	}
 	
 	
